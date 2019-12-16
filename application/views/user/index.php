1de68c9bc1c393886e08662bdd77a78afe1582b1
garuda210 <173040044@mail.unpas.ac.id>
2019-12-16 11:28:10 +0700

    merapihkan halaman index user

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -3,7 +3,7 @@
                 <div class="container-fluid">
                     <!-- Page Heading -->
                     <div class="row">
-                        <?php foreach ($pakaian as $p) : ?> <div class="det-cards"> <img src="<?= base_url('assets/img/pakaian/') . $p['Gambar_Pakaian'] ?>" class="image-cards" style="width:100%">
+                        <?php foreach ($pakaian as $p) : ?> <div class="det-cards"> <img src="<?= base_url('assets/img/pakaian/') . $p['Gambar_Pakaian'] ?>" class="image-cards" style="width:100% ;height : 450px">
                                 <div class="container-card col-lg-5">
                                     <h6><b><?= $p['Nama_Pakaian'] ?></b></h6>
                                     <p>IDR. <?= number_format($p["Harga_Pakaian"], 2, ',', '.'); ?></p>
